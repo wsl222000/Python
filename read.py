3f4a0d750b35f9d7db1f9c50f4b3155b266a53f3
Jack Wu <wsl222000@gmail.com>
2015-06-03 11:04:22 +0800

    read.py
    usage of read file in python

---
+++ read.py
@@ -0,0 +1,18 @@
+读取文件一行
+#!/usr/bin/python
+f=open('/etc/hosts')
+line=f.readline()
+print line,
+f.close()
+
+读取整个文件
+#!/usr/bin/python
+import sys
+f=open('/etc/hosts')
+for line in f:
+  print line,
+f.close()
+
+#!/usr/bin/python
+for i in open('/etc/hosts'):
+  print i,
